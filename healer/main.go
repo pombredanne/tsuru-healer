ea7f10377995bb981a22578d17d6604908ca2ef6
Flavia Missi <flaviamissi@gmail.com>
2013-01-10 18:02:41 -0200

    go fmt ./healer/main.go

--- healer/main.go
+++ healer/main.go
@@ -3,27 +3,27 @@
 package main
 
 import (
-    "fmt"
-    "github.com/globocom/tsuru-healer/healer"
+	"fmt"
+	"github.com/globocom/tsuru-healer/healer"
+	"log/syslog"
 	"os"
-    "time"
-    "log/syslog"
+	"time"
 )
 
 func main() {
-    var log *syslog.Writer
-    if len(os.Args) < 3 {
-        fmt.Println("Healer expects email, password and endpoint to connect with tsuru.")
-        return
-    }
+	var log *syslog.Writer
+	if len(os.Args) < 3 {
+		fmt.Println("Healer expects email, password and endpoint to connect with tsuru.")
+		return
+	}
 	email := os.Args[0]
 	password := os.Args[1]
 	endpoint := os.Args[2]
 	healer := healer.NewTsuruHealer(email, password, endpoint)
-    for _ = range time.Tick(time.Minute) {
-        err := healer.Heal()
-        if err != nil {
-            log.Err("Got error while healing: " + err.Error())
-        }
-    }
+	for _ = range time.Tick(time.Minute) {
+		err := healer.Heal()
+		if err != nil {
+			log.Err("Got error while healing: " + err.Error())
+		}
+	}
 }
