2bf356c924c2a1fed36874ab0f58be6a5f4849f0
Andrews Medina <andrewsmedina@gmail.com>
2013-01-22 18:40:33 -0200

    Fixed race conditions.

--- healer/tickers_test.go
+++ healer/tickers_test.go
@@ -4,13 +4,14 @@ import (
 	. "launchpad.net/gocheck"
 	"net/http"
 	"net/http/httptest"
+	"sync/atomic"
 	"time"
 )
 
 func (s *S) TestHealTicker(c *C) {
-	var called bool
+	var called int32
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		called = true
+		atomic.StoreInt32(&called, 1)
 	}))
 	defer ts.Close()
 	h := &tsuruHealer{url: ts.URL}
@@ -25,13 +26,13 @@ func (s *S) TestHealTicker(c *C) {
 	time.Sleep(1 * time.Second)
 	close(ch)
 	<-ok
-	c.Assert(called, Equals, true)
+	c.Assert(atomic.LoadInt32(&called), Equals, int32(1))
 }
 
 func (s *S) TestRegisterTicker(c *C) {
-	var called bool
+	var called int32
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		called = true
+		atomic.StoreInt32(&called, 1)
 	}))
 	defer ts.Close()
 	ch := make(chan time.Time)
@@ -43,6 +44,5 @@ func (s *S) TestRegisterTicker(c *C) {
 	ch <- time.Now()
 	time.Sleep(1 * time.Second)
 	close(ch)
-	<-ok
-	c.Assert(called, Equals, true)
+	c.Assert(atomic.LoadInt32(&called), Equals, int32(1))
 }
