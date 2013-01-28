66ffdc85f2f8866bfb8132be3bb33a3fb236de44
Andrews Medina <andrewsmedina@gmail.com>
2013-01-28 18:06:35 -0200

    Using wait groups.

--- healer/tickers.go
+++ healer/tickers.go
@@ -1,25 +1,38 @@
 package main
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // healTicker execute the healers.heal.
 func healTicker(ticker <-chan time.Time) {
+	var wg sync.WaitGroup
 	for _ = range ticker {
 		healers := getHealers()
-		for _, healer := range healers {
-			go healer.heal()
+		wg.Add(len(healers))
+		for _, h := range healers {
+			go func(healer healer) {
+				healer.heal()
+				wg.Done()
+			}(h)
 		}
+		wg.Wait()
 	}
 }
 
 // registerTicker register healers from resource.
 func registerTicker(ticker <-chan time.Time, endpoint string) {
+	var registerHealer = func() {
+		healers, _ := healersFromResource(endpoint)
+		for name, healer := range healers {
+			register(name, &healer)
+		}
+	}
+	registerHealer()
 	go func() {
 		for _ = range ticker {
-			healers, _ := healersFromResource(endpoint)
-			for name, healer := range healers {
-				register(name, &healer)
-			}
+			registerHealer()
 		}
 	}()
 }
