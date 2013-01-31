c1b4c65a99317fe7c5d84d2368b4f566089e1049
Andrews Medina <andrewsmedina@gmail.com>
2013-01-31 09:31:28 -0200

    Making a copy of healers for fix a bug.

--- healer/tickers.go
+++ healer/tickers.go
@@ -2,32 +2,28 @@ package main
 
 import (
 	"fmt"
-	/* "sync" */
+	"sync"
 	"time"
 )
 
 // healTicker execute the healers.heal.
 func healTicker(ticker <-chan time.Time) {
 	log.Info("running heal ticker")
-	/* var wg sync.WaitGroup */
+	var wg sync.WaitGroup
 	for _ = range ticker {
 		healers := getHealers()
-		/* wg.Add(len(healers)) */
+		wg.Add(len(healers))
 		for name, h := range healers {
 			log.Info(fmt.Sprintf("running verification/heal for %s", name))
-			err := h.heal()
-			if err != nil {
-				log.Info(err.Error())
-			}
-			// go func(healer healer) {
-			// 	err := healer.heal()
-			// 	if err != nil {
-			// 		log.Info(err.Error())
-			// 	}
-			// 	wg.Done()
-			// }(h)
+			go func(healer healer) {
+				err := healer.heal()
+				if err != nil {
+					log.Info(err.Error())
+				}
+				wg.Done()
+			}(h)
 		}
-		/* wg.Wait() */
+		wg.Wait()
 	}
 }
 
@@ -37,7 +33,8 @@ func registerTicker(ticker <-chan time.Time, endpoint string) {
 		log.Info("running register ticker")
 		healers, _ := healersFromResource(endpoint)
 		for name, healer := range healers {
-			register(name, &healer)
+			copy := healer
+			register(name, &copy)
 		}
 	}
 	registerHealer()
